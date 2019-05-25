54888612ef98ce2a1f5d5d048c4d8ae190797bbe
VrX55 <46119849+VrX55@users.noreply.github.com>
2019-05-25 16:28:48 +0700

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,361 @@
+#-*-coding:utf-8-*-
+#menyambut bulan suci ramadhan
+# coding by Dani/VrX
+import sys
+import os
+import time
+# color
+################
+N = '\033[0m'
+W = '\033[1;37m'
+B = '\033[1;34m'
+M = '\033[1;35m'
+R = '\033[1;31m'
+G = '\033[1;32m'
+Y = '\033[1;33m'
+C = '\033[1;36m'
+I = '\033[1;3m'
+################
+pareasi ="""                                       
+%s  ___         _        _ _           
+ |_ _|_ _  __| |_ __ _| | |___ _ _%sv%s2.5
+%s  | || ' \(_-<  _/ _` | | / -_) '_|  %s author:V%s[R]%sX
+%s |___|_||_/__/\__\__,_|_|_\___|_|%s code%s[N]%same:%s[D]%saniKun%s(c)2019
+%s+----+------------------------------------------------------+
+|    |              D A F T A R  M E N U                    |
++----+------------------------------------------------------+
+| 01 | TOOLKIT TUAN B4DUT                                   |
++----+------------------------------------------------------+
+| 02 | B4J1N64Nv5                                           |
++----+------------------------------------------------------+
+| 03 | Lazymux                                              |
++----+------------------------------------------------------+
+| 04 | Tool Mr.Cakil                                        |
++----+------------------------------------------------------+
+| 05 | Report Account Facebook                              |
++----+------------------------------------------------------+
+| 06 | Multi Brute Force2(MBF2)                             |
++----+------------------------------------------------------+
+| 07 | FBshield                                             |
++----+------------------------------------------------------+
+| 08 | Ultimate-Dork                                        |
++----+------------------------------------------------------+
+| 09 | ReconT                                               |
++----+------------------------------------------------------+
+| 10 | Zeebsploit                                           |
++----+------------------------------------------------------+
+| 11 | SQLMAP                                               |
++----+------------------------------------------------------+
+| 12 | Metasploit-framework                                 |
++----+------------------------------------------------------+
+| 13 | Facebook Brute Force                                 |
++----+------------------------------------------------------+
+| 14 | Encrypt Your File Python                             |
++----+------------------------------------------------------+
+| 15 | SPAMMER GRAB                                         |
++----+------------------------------------------------------+
+| 16 | SPAMMER BUKALAPAK FOR WA                             |
++----+------------------------------------------------------+
+| 17 | Yahoo Cloning (Wahyu Andhika)                        |
++----+------------------------------------------------------+
+| 18 | Cloning Yahoo (GUNAWAN ID)                           | 
++----+------------------------------------------------------+
+| 19 | About                                                |
++----+------------------------------------------------------+
+| 00 | Exit                                                 |
++----+------------------------------------------------------+"""%(R,W,W,R,B,W,B,R,B,W,B,W,B,W,G)
+ket ="""
+CARA PEMAKAIANNYA MUDAH KOK:V
+PILIH SALAH SATU DI MENU TABEL :V
+TERUS TUNGGU SAMPAI SUCCESS TERINSTALL...
+SESUDAH ITU KAN KEMBALI KE MENU NNTI PILIH
+EXIT ATAU NOMER 0 OTOMATIS KELUAR
+DAN KETIK 'ls' TANPA TANDA KUTIP NAH 
+PILIH FOLDER YANG TADI LU UDAH INSTALL
+MASIH BINGGUNG?? MIKIR DONG :'V
+%s
+DibuatOleh:VrX
+CodeName: Dani Kun
+Terima Kasih Kpd:
+-Allah Swt
+-Pembuat tools
+-Kalian Semua
+
+sumber:
+https://google.com
+https://github.com
+https://github.com/TUANB4DUT
+https://github.com/DarknessCyberTeam
+https://github.com/Gameye98
+https://github.com/mrcakil
+https://github.com/herp1337
+https://github.com/jaxBCD
+https://github.com/sqlmapproject
+https://github.com/rapid7
+https://github.com/VrX55
+https://github.com/DarkCurut08
+https://github.com/hatakecnk
+https://github.com/GUNAWAN18ID
+https://github.com/wahyuandhika
+%s
+
+"""%(W,W)
+def badut():
+	print "%splease wait..."%(G)
+	time.sleep(5)
+	os.system("git clone https://github.com/TUANB4DUT/TOOLKIT")
+	print "%ssuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+
+def bajingan():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+
+def lazymux():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/Gameye98/Lazymux")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+
+def cakil():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/mrcakil/Mrcakil.git")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+
+def report():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/herp1337/repoort")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+	
+def mbf():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/herp1337/MBF2")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+	
+def shield():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/jaxBCD/FBshield")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+	
+def dork():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/jaxBCD/Ultimate-Dork")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+	
+def recont():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/jaxBCD/ReconT")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+
+def zeebsploit():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/jaxBCD/Zeebsploit")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+	
+def sqlmap():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/sqlmapproject/sqlmap")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+
+def metasploit():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/rapid7/metasploit-framework")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+	
+def brute():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/VrX55/FB-Brutef")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+	
+def encrypt():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/hatakecnk/CompileMarshal")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+
+
+def call():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/VrX55/jahil")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+
+def buka():
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/DarkCurut08/spam-wa")
+	print "%sSuccess%s"%(G,W)
+	time.sleep(5)
+	os.system("clear")
+	return menu()
+def about():
+	print " "
+	print ket
+	t = raw_input("[+] kembali ke menu?[y/n]: ")
+	if t =="y" or t =="Y":
+		return menu()
+	elif t =="n" or t =="N":
+		print "Terima Kasih"
+		time.sleep(1)
+		print "Wassalamualikum Wr.Wb"
+		time.sleep(2)
+		sys.exit()
+	else:
+		print ("%sinput failed%s"%(R,W))
+		time.sleep(2)
+		os.system("clear")
+		return about()
+def wahyu():
+	print " "
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/wahyuandhika/YahooCloning")
+	print "%ssuccess%s"%(G,W)
+	time.sleep(2)
+	return menu()
+def gunawan():
+	print " "
+	print "%splease wait...%s"%(G,W)
+	time.sleep(5)
+	os.system("git clone https://github.com/GUNAWAN18ID/cloning-yahoo")
+	print "%ssuccess%s"%(G,W)
+	time.sleep(2)
+	return menu()
+	
+    	
+def menu():
+    os.system("clear")
+    print pareasi
+    os.system("date")
+    print (" ")
+    user = input("%s[%s+%s] MASUKAN PILIHAN ANDA : "%(W,R,W))
+    if user ==1:
+    	time.sleep(2)
+        badut()
+    elif user ==2:
+    	time.sleep(3)
+    	bajingan()
+    elif user ==3:
+    	time.sleep(3)
+    	lazymux()
+    elif user ==4:
+    	time.sleep(3)
+    	cakil()
+    elif user ==5:
+    	time.sleep(3)
+    	report()
+    elif user ==6:
+    	time.sleep(3)
+    	mbf()
+    elif user ==7:
+    	time.sleep(3)
+    	shield()
+    elif user ==8:
+    	time.sleep(3)
+    	dork()
+    elif user ==9:
+    	time.sleep(3)
+    	recont()
+    elif user ==10:
+    	time.sleep(3)
+    	zeebsploit()
+    elif user ==11:
+    	time.sleep(3)
+    	sqlmap()
+    elif user ==12:
+    	time.sleep(3)
+    	metasploit()
+    elif user ==13:
+    	time.sleep(3)
+    	brute()
+    elif user ==14:
+    	time.sleep(3)
+    	encrypt()
+    elif user ==15:
+    	time.sleep(3)
+    	call()
+    elif user ==16:
+    	time.sleep(3)
+    	buka()
+    elif user ==17:
+    	wahyu()
+    elif user ==18:
+    	gunawan()
+    elif user ==19:
+    	time.sleep(3)
+    	os.system("clear")
+    	about()
+    elif user ==0:
+    	time.sleep(1)
+    	print ("%s-Bye Bye Ster ^^"%(G))
+    	time.sleep(2)
+    	print ("-Doakan Aku Sukses Y >///<")
+    	time.sleep(1)
+    	print ("-AMIN AMIN AMIN")
+    	time.sleep(2)
+    	print ("-Nikmati Toolnya ^_^")
+    	print ("-Terima Kasih%s"%(W))
+    	time.sleep(1)
+        sys.exit()
+    else:
+        print ("%s[!] input failed !!!%s"%(R,W))
+        time.sleep(1)
+        return menu()
+if __name__=="__main__":
+    menu()
